deaea403023793c24f65354336a874c5a29799dc
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-02-04 16:27:58 +0000

    Dice Game single player ending

--- script.js
+++ script.js
@@ -12,4 +12,24 @@ function rollTheDice(){
         score += randomNumber;
         document.getElementById("scoreShow").textContent = score;
         image.style.visibility = "visible";
+
+        if (randomNumber == 1) {
+            document.getElementById("head").textContent = "You Lose!";
+            document.getElementById("sobutn").style.visibility ="visible";
+            document.getElementById("roll").style.visibility ="hidden";
+        }
+        if (score >= 20) {
+            document.getElementById("head").textContent = "You Win!";
+            document.getElementById("sobutn").style.visibility = "visible";
+            document.getElementById("roll").style.visibility ="hidden";
+        }
     }
+
+function startOver(){
+    score= 0;
+    document.getElementById("scoreShow").textContent = "0";
+    document.getElementById("head").textContent = "Player 1";
+    document.getElementById("die").style.visibility = "hidden";
+    document.getElementById("sobutn").style.visibility ="hidden";
+    document.getElementById("roll").style.visibility ="visible";
+}
